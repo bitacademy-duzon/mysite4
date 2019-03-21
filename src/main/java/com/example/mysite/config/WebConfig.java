90fdce750312640de16f196e252ae44ef52bd151
BIT <BIT@DESKTOP-4M0FKCM>
2019-03-21 15:55:47 +0900

    completed

--- src/main/java/com/example/mysite/config/WebConfig.java
+++ src/main/java/com/example/mysite/config/WebConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import com.example.config.web.FileUploadConfig;
 import com.example.config.web.MVCConfig;
 import com.example.config.web.MessagesConfig;
 import com.example.config.web.SecurityConfig;
 
 @Configuration
-@ComponentScan("com.example.mysite.controller")
-@Import(value= {MVCConfig.class, SecurityConfig.class, MessagesConfig.class})
+@ComponentScan(value= {"com.example.mysite.controller", "com.example.mysite.exception"})
+@Import(value= {MVCConfig.class, SecurityConfig.class, MessagesConfig.class, FileUploadConfig.class})
 public class WebConfig {
 }
