e86307fe6e6818b6ddb6d544bec5c4c0af617ef6
BIT <BIT@DESKTOP-4M0FKCM>
2019-03-22 09:14:39 +0900

    AOP 적용

--- src/main/java/com/example/mysite/config/AppConfig.java
+++ src/main/java/com/example/mysite/config/AppConfig.java
@@ -2,13 +2,15 @@ package com.example.mysite.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 import com.example.config.app.DBConfig;
 import com.example.config.app.MyBatisConfig;
 
 @Configuration
-@ComponentScan(value= {"com.example.mysite.service", "com.example.mysite.repository"})
+@EnableAspectJAutoProxy
+@ComponentScan(value= {"com.example.mysite.service", "com.example.mysite.repository", "com.example.mysite.aspect"})
 @Import(value={ DBConfig.class, MyBatisConfig.class })
 public class AppConfig {
 }
--- src/main/java/com/example/mysite/config/WebConfig.java
+++ src/main/java/com/example/mysite/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.mysite.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 import com.example.config.web.FileUploadConfig;
@@ -10,7 +11,8 @@ import com.example.config.web.MessagesConfig;
 import com.example.config.web.SecurityConfig;
 
 @Configuration
-@ComponentScan(value= {"com.example.mysite.controller", "com.example.mysite.exception"})
+@EnableAspectJAutoProxy
+@ComponentScan(value= {"com.example.mysite.controller", "com.example.mysite.exception" })
 @Import(value= {MVCConfig.class, SecurityConfig.class, MessagesConfig.class, FileUploadConfig.class})
 public class WebConfig {
 }
